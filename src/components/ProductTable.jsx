701289539198ae21c77b819e4fcaaa64893756df
richa68 <richamhshwr2@gmail.com>
2025-07-15 22:51:39 +0530

    remove search bar

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -244,14 +244,14 @@ const ProductTable = () => {
 
   return (
     <div className="p-4 w-full bg-neutral-100 rounded-sm">
-      <div className="flex flex-wrap items-center justify-between gap-4 mb-4">
-        <input
+      <div className="flex flex-wrap items-end justify-end gap-4 mb-4">
+        {/* <input
           type="text"
           placeholder="Search by name or category..."
           value={localSearch}
           onChange={(e) => setLocalSearch(e.target.value)}
           className="border px-3 py-2 rounded shadow-sm w-full md:w-1/3"
-        />
+        /> */}
 
         <div className="flex gap-1 bg-blue-100">
           <select
